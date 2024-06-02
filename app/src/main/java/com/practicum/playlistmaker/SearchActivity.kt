32f4150ddddf8186f30c292b05ac07119e7b0631
malenkii-myk <admin.miha@gmail.com>
2024-06-02 15:11:03 +0300

    задания 9-ого спринта - не понял про кнопки-стрелки "Назад", в задании про них говорится, но в макете Figm'e их нет (хотя в самом стартовом одна кнопка была). В общем кнопки из проекта удалил до последующих разъяснений. - не понял как менять цвета на Switch'ере

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -1,11 +1,81 @@
 package com.practicum.playlistmaker
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.ImageView
 
 class SearchActivity : AppCompatActivity() {
+
+    companion object {
+        const val SEARCH_VALUE = "SEARCH_VALUE"
+        const val SEARCH_DEF = ""
+    }
+
+    private var searchValue: String = SEARCH_DEF
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(SEARCH_VALUE, searchValue)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        searchValue = savedInstanceState.getString(SEARCH_VALUE, SEARCH_DEF)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val inputSearchText = findViewById<EditText>(R.id.input_search)
+        val clearButton = findViewById<ImageView>(R.id.btn_search_clear)
+        val searchView = findViewById<View>(R.id.search_activity)
+
+        inputSearchText.setText(searchValue)
+
+        fun View.hideKeyboard() {
+            val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(windowToken, 0)
+        }
+
+        // clear button
+        clearButton.setOnClickListener {
+            inputSearchText.setText("")
+            searchView.hideKeyboard()
+        }
+
+        val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // empty
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                clearButton.visibility = if (s.isNullOrEmpty()) View.GONE else View.VISIBLE
+                searchValue = s.toString()
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // empty
+            }
+        }
+        inputSearchText.addTextChangedListener(simpleTextWatcher)
+
+
+        // button Назад
+//        val btnBackToMainActivity = findViewById<ImageButton>(R.id.btn_back_to_main)
+//        btnBackToMainActivity.setOnClickListener {
+//            startActivity(
+//                Intent(this, MainActivity::class.java)
+//            )
+//        }
+
     }
+
+
 }
--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -1,8 +1,12 @@
 package com.practicum.playlistmaker
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.PreferenceFragmentCompat
@@ -14,13 +18,43 @@ class SettingsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.settings_activity)
 
-        // button Назад
-        val btnBackToMainActivity = findViewById<ImageButton>(R.id.btn_back_to_main)
-        btnBackToMainActivity.setOnClickListener {
-            startActivity(
-                Intent(this, MainActivity::class.java)
-            )
+        // button Share
+        val btnShare = findViewById<View>(R.id.btn_share)
+        btnShare.setOnClickListener {
+            val i = Intent(Intent.ACTION_SENDTO)
+            i.putExtra(Intent.EXTRA_TEXT, getString(R.string.app_name))
+            i.type = "text/plain"
+            startActivity( i )
         }
+
+        // button Support
+        val btnSupport = findViewById<View>(R.id.btn_support)
+        btnSupport.setOnClickListener {
+            val i = Intent(Intent.ACTION_SENDTO)
+            i.data = Uri.parse("mailto:")
+            i.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.support_mail_subject))
+            i.putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.support_mail_to)))
+            i.putExtra(Intent.EXTRA_TEXT, getString(R.string.support_mail_text))
+            startActivity( i )
+        }
+
+        // button License
+        val btnLicense = findViewById<View>(R.id.btn_license)
+        btnLicense.setOnClickListener {
+            val i = Intent(Intent.ACTION_VIEW)
+            i.data = Uri.parse( getString(R.string.offer_url) )
+            startActivity( i )
+        }
+
+        // button Назад
+//        val btnBackToMainActivity = findViewById<ImageButton>(R.id.btn_back_to_main)
+//        btnBackToMainActivity.setOnClickListener {
+//            startActivity(
+//                Intent(this, MainActivity::class.java)
+//            )
+//        }
+
+
     }
 
 
