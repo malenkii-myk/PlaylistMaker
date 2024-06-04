c72dd32b8ec8c3a41ea33072bd658e360b955431
malenkii-myk <admin.miha@gmail.com>
2024-06-04 19:17:44 +0300

    задания 10-ого спринта

---
+++ app/src/main/java/com/practicum/playlistmaker/TrackAdapter.kt
@@ -0,0 +1,23 @@
+package com.practicum.playlistmaker
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class TrackAdapter(
+    private val tracks: ArrayList<Track>
+) : RecyclerView.Adapter<TracksViewHolder> () {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TracksViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.track_view, parent, false)
+        return TracksViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: TracksViewHolder, position: Int) {
+        holder.bind(tracks[position])
+    }
+
+    override fun getItemCount(): Int {
+        return tracks.size
+    }
+}
--- app/src/main/java/com/practicum/playlistmaker/Utils.kt
+++ app/src/main/java/com/practicum/playlistmaker/Utils.kt
@@ -1,8 +1,16 @@
 import android.content.Context
+import android.util.TypedValue
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 
 fun View.hideKeyboard() {
     val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
     imm.hideSoftInputFromWindow(windowToken, 0)
+}
+
+fun dpToPx(dp: Float, context: Context): Int {
+    return TypedValue.applyDimension(
+        TypedValue.COMPLEX_UNIT_DIP,
+        dp,
+        context.resources.displayMetrics).toInt()
 }
