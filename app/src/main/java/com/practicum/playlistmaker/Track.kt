c55fc165cb722f32c3e7332302f59ab820e54e12
mike <admin.miha@gmail.com>
2024-11-17 01:36:34 +0800

    11

--- app/src/main/java/com/practicum/playlistmaker/Track.kt
+++ app/src/main/java/com/practicum/playlistmaker/Track.kt
@@ -1,11 +1,19 @@
 package com.practicum.playlistmaker
 
+import java.text.SimpleDateFormat
+import java.util.Locale
+
 data class Track(
     val trackName: String, // Название композиции
     val artistName: String, // Имя исполнителя
-    val trackTime: String, // Продолжительность трека
+    val trackTimeMillis: Long, // Продолжительность трека
     val artworkUrl100: String, // Ссылка на изображение обложки
 ){
+
+    fun getFormattedTrackTime(): String {
+        return SimpleDateFormat("mm:ss", Locale.getDefault()).format(trackTimeMillis)
+    }
+
     companion object {
         val IMG_HOST: String = "https://is5-ssl.mzstatic.com/image/thumb/"
     }
---
+++ app/src/main/java/com/practicum/playlistmaker/TrackAPI.kt
@@ -0,0 +1,13 @@
+package com.practicum.playlistmaker
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+
+interface trackApi {
+    @GET("/search?entity=song")
+    fun search(
+        @Query("term") text: String
+    ) : Call<TrackResponse>
+}
